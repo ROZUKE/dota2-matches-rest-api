3c5269b4156d635bc2dd6c7ad3917941590b0543
ROZUKE <rodrigo.sulca499@gmail.com>
2023-03-23 19:59:08 -0400

    Initial commit

---
+++ src/main/java/com/dota2/rozuke/RozukeApplication.java
@@ -0,0 +1,13 @@
+package com.dota2.rozuke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RozukeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RozukeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dota2/rozuke/RozukeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dota2.rozuke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RozukeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
